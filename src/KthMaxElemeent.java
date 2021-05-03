d4de5a1395f54616fc49d23b1c44450f29fc4410
Olx-user <olx-user@Sysops-MacBook-Pro.local>
2021-05-03 16:56:58 +0530

    Kth max element - Heap

---
+++ src/KthMaxElemeent.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class KthMaxElemeent {
+    public static void main(String[] args) {
+        KthMaxElemeent element = new KthMaxElemeent();
+        element.findKthLargest(new int[]{3,2,3,1,2,4,5,5,6},9);
+    }
+    public void findKthLargest(int[] nums, int k) {
+        int[] arr = new int[k+1];
+
+        for(int i=0; i<k ;i++){
+            arr[i+1] = nums[i];
+        }
+
+        buildHeap(arr, k);
+
+        System.out.println("nums = " + Arrays.toString(arr) + ", k = " + k );
+
+        int kthMax = kthMax(arr, 4, k);
+
+        System.out.println("nums = " + Arrays.toString(arr) + ", k = " + k + ", Kth max = " + kthMax);
+    }
+
+    private int kthMax(int[] arr, int k , int size) {
+        for(int i = 0 ; i < k-1; i++){
+            swap(arr,1,size);
+            size = size - 1;
+            maxHeapify(arr,1,size);
+        }
+        return arr[1];
+    }
+
+    private void maxHeapify(int[] a , int index, int size){
+        int left = 2 * index;
+        int right = 2 * index + 1;
+        int largest;
+        if(left<=size && a[left] > a[index]){
+            largest = left;
+        }
+        else{
+            largest = index;
+        }
+
+        if(right<=size && a[right] > a[largest]){
+            largest = right;
+        }
+
+        if (index != largest ){
+            swap(a,largest,index);
+            maxHeapify(a,largest,size);
+        }
+    }
+
+    private void buildHeap(int[] nums, int k){
+        for(int i = k/2 ;i >=1 ; i--){
+            maxHeapify(nums,i,k);
+        }
+    }
+
+    private void swap(int[] a , int x, int y){
+        int temp = a[x];
+        a[x] = a[y];
+        a[y] = temp;
+    }
+}
