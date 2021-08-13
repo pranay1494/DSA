00b12691ecb542f08dc7a38179b34a2d9ec02690
= <pranaybansal@Sysops-MacBook-Pro.local>
2021-08-14 02:54:56 +0530

    MinimumSizeSubarraySum

---
+++ src/slidingwindow/LongestSubstringwithKDistinctCharacters.java
@@ -0,0 +1,22 @@
+//package slidingwindow;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//public class LongestSubstringwithKDistinctCharacters {
+//    public static void main(String[] args) {
+//
+//    }
+//
+//    private int solve(String input, int k){
+//        Map<Character,Integer> frequency = new HashMap<>();
+//        int start = 0;
+//        int windowSum = 0;
+//        int ans = Integer.MAX_VALUE;
+//
+//        for (int end = 0; end < input.length(); end++) {
+//            frequency.put(input.charAt(end), frequency.get(input.charAt(end))+1);
+//
+//        }
+//    }
+//}
--- src/slidingwindow/MinimumSizeSubarraySum.java
+++ src/slidingwindow/MinimumSizeSubarraySum.java
@@ -4,7 +4,7 @@ package slidingwindow;
 public class MinimumSizeSubarraySum {
     public static void main(String[] args) {
         MinimumSizeSubarraySum sum = new MinimumSizeSubarraySum();
-        System.out.println("args = " + sum.minSubArrayLen(11,new int[]{1,1,1,1,1,1,1,1}));
+        System.out.println("args = " + sum.minSubArrayLen(7, new int[]{2, 1, 5, 2, 3, 2}));
     }
 
     public int minSubArrayLen(int target, int[] nums) {
@@ -16,14 +16,14 @@ public class MinimumSizeSubarraySum {
         for (int i = 0; i < nums.length; i++) {
             windowSum += nums[i];
             end = i;
-            if (windowSum + nums[i] >= target){
-                //shrink
-                while(windowSum >= target){
-                    ans = Math.min(end-start+1,ans);
-                    windowSum-= nums[start];
-                    start++;
-                }
+
+            //shrink
+            while (windowSum >= target) {
+                ans = Math.min(end - start + 1, ans);
+                windowSum -= nums[start];
+                start++;
             }
+
         }
         if (ans == Integer.MAX_VALUE) {
             return 0;
