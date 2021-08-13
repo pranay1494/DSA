3966d150a57f63308537bcac315bbe98dc72d729
= <pranaybansal@Sysops-MacBook-Pro.local>
2021-08-14 03:49:47 +0530

    LongestSubstringwithKDistinctCharacters

--- src/slidingwindow/LongestSubstringwithKDistinctCharacters.java
+++ src/slidingwindow/LongestSubstringwithKDistinctCharacters.java
@@ -1,22 +1,42 @@
-//package slidingwindow;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//public class LongestSubstringwithKDistinctCharacters {
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    private int solve(String input, int k){
-//        Map<Character,Integer> frequency = new HashMap<>();
-//        int start = 0;
-//        int windowSum = 0;
-//        int ans = Integer.MAX_VALUE;
-//
-//        for (int end = 0; end < input.length(); end++) {
-//            frequency.put(input.charAt(end), frequency.get(input.charAt(end))+1);
-//
-//        }
-//    }
-//}
+package slidingwindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstringwithKDistinctCharacters {
+    public static void main(String[] args) {
+        LongestSubstringwithKDistinctCharacters characters = new LongestSubstringwithKDistinctCharacters();
+        System.out.println("args = " + characters.longestkSubstr("aaaaaa",2));
+    }
+
+    private int longestkSubstr(String input, int k){
+        Map<Character,Integer> frequency = new HashMap<>();
+        int start = 0;
+        int ans = -1;
+
+        if (isDistinctAvailable(input) < k) return -1;
+
+        for (int end = 0; end < input.length(); end++) {
+            frequency.put(input.charAt(end), frequency.getOrDefault(input.charAt(end),0)+1);
+            while(frequency.size() > k){
+                // shrink
+                if (frequency.get(input.charAt(start)) == 1){
+                    frequency.remove(input.charAt(start));
+                }else{
+                    frequency.put(input.charAt(start), frequency.get(input.charAt(start)) - 1);
+                }
+                start++;
+            }
+            ans = Math.max(ans,end - start + 1);
+        }
+        return ans;
+    }
+
+    int isDistinctAvailable(String s){
+        Map<Character,Integer> frequency = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            frequency.put(s.charAt(i),1);
+        }
+        return frequency.size();
+    }
+}
